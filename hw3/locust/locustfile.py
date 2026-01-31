7f3b3de68e1611e6a8f1fe069187f6b8827388f7
YaoyiW27 <rmwyyy@gmail.com>
2026-01-31 15:04:23 -0800

    finish locust part and local test

---
+++ hw3/locust/locustfile.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between
+
+class AlbumUser(HttpUser):
+    wait_time = between(1, 2)  # Wait 1-2 seconds between tasks
+
+    @task(3)  # Weight 3 - GET will run 3x more often than POST
+    def get_albums(self):
+        self.client.get("/albums")
+
+    @task(1)  # Weight 1
+    def post_album(self):
+        self.client.post("/albums", json={
+            "id": "99",
+            "title": "Test Album",
+            "artist": "Test Artist",
+            "price": 9.99
+        })
